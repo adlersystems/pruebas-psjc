af0e8f059f73a1f4223b5020c654606d82628929
unknown <adlersystems@gmail.com>
2015-03-25 13:40:40 -0600

    Update script updated

--- php/update.php
+++ php/update.php
@@ -6,6 +6,9 @@
   $xml=simplexml_load_file("config.xml");
   $newVersion = trim($xml->version);
   
+  echo $phoneAppVersion ."<br />";
+  echo $newVersion ."<br /><br />";
+  
   if($phoneAppVersion != $newVersion){
 	  echo '<a href="http://www.parroquiasjc.org/app/update.html" class="ui-btn ui-mini ui-corner-all ui-btn-inline ui-icon-recycle ui-btn-icon-left ui-btn-f">Actualizaci&oacute;n disponible</a>';
   }
